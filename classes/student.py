8c409932b126c0d2209dd2753ece2f0f81ed1f90
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-02 16:12:40 -0400

    works

--- classes/student.py
+++ classes/student.py
@@ -3,6 +3,7 @@ import csv
 
 class Student(Person):
 
+    student_list = []
 
     def __init__(self, name, age, role, school_id, password):
         super().__init__(name, age, role, password)
@@ -11,12 +12,22 @@ class Student(Person):
     @classmethod
     def all_students(cls):
         file_name = './data/students.csv'
-        student_list = []
+        
 
         with open(file_name, newline='') as student_file:
             reader = csv.DictReader(student_file)
 
             for row in reader:
-                student_list.append(row)
+                cls.student_list.append(row)
 
-        return student_list
+        return cls.student_list
+
+    @classmethod
+    def add_student(cls, name, age, role, school_id, password):
+        cls.student_list.append({'name': name, 'age': age, 'role': role, 'school_id': school_id, 'password': password})
+
+    @classmethod
+    def remove_student(cls, school_id):
+        for i,item in enumerate(Student.student_list):
+            if school_id == item['school_id']:
+                Student.student_list.pop(i)
--- runner.py
+++ runner.py
@@ -14,10 +14,56 @@ from classes.student import Student
 
 # print(*Student.all_students(), sep='\n')
 
-from classes.school import School 
+# from classes.school import School 
 
 school = School('Ridgemont High') 
-print('\n*****STAFF*****')
-print(*school.staff, sep='\n') 
-print('\n\n*****STUDENTS*****')
-print(*school.students, sep = '\n')
+# print('\n*****STAFF*****')
+# print(*school.staff, sep='\n') 
+# print('\n\n*****STUDENTS*****')
+# print(*school.students, sep = '\n')
+
+options_arr = [1, 2, 3, 4, 5]
+bool_var = True
+
+while(bool_var):
+    user_input = input("""
+Ridgemont High Student Interface 
+--------------------------------
+Welcome, Richard. Your access level is Principal
+    What would you like to do?
+    Options:
+    1 List All Students
+    2 View Individual Student <student_id>
+    3 Add a Student
+    4 Remove a Student <student_id>
+    5 Quit
+\n\n>>""")
+    if user_input == '1':
+        print('\n\n*****STUDENTS*****')
+        print(*school.students, sep = '\n') 
+    elif user_input == '2':
+        id = input('Enter a valid student ID:\n>>')
+        check = next((item for item in school.students if item['school_id'] == id), 'there is no student with that id')
+        print(check)
+
+    elif user_input == '3':
+        name = input('Please enter the student name: ')
+        age = input('Please enter the student age: ')
+        role = 'Student'
+        school_id = input('Please enter the student ID: ')
+        password = input('Please enter a temporary password: ')
+
+        Student.add_student(name, age, role, school_id, password)
+        print('Student added to the database.')
+
+    elif user_input == '4':
+        id = input('Enter a valid student ID:\n>>')
+        next((Student.remove_student(id) for item in school.students if item['school_id'] == id), 'there is no student with that id')
+        
+    elif user_input == '5':
+        bool_var = False
+
+    else:
+        user_input = input('Please read the directions. Hit enter to continue.')
+
+
